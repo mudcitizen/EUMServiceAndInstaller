4f3c02f84bad2e351941eefa2760534b238c2173
MudCitizen <mudcitizen@gmail.com>
2017-10-26 13:05:35 -0400

    Add test project for Config

---
+++ PSMS.Host.FileServerConnectionManager/PSMS.Host.FileServerConnectionManager.Config.Test/Experiments.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PSMS.Host.FileServerConnectionManager.Config.Test
+{
+    [TestClass]
+    public class Experiments
+    {
+        [TestMethod]
+        public void TestMessageBox()
+        {
+            DialogResult dr = MessageBox.Show("VFP Dataset?", "Caption", MessageBoxButtons.YesNoCancel);
+            String txt;
+            switch (dr)
+            {
+                case DialogResult.Yes:
+                    {
+                        txt = "Yes";
+                        break;
+                    }
+                case DialogResult.No:
+                    {
+                        txt = "No";
+                        break;
+                    }
+                default:   
+                    {
+                        txt = "default / cancel";
+                        break;
+                    }
+            }
+
+            Debug.WriteLine(txt);
+            Debug.WriteLine("Done");
+
+        }
+    }
+}
---
+++ PSMS.Host.FileServerConnectionManager/PSMS.Host.FileServerConnectionManager.Config.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PSMS.Host.FileServerConnectionManager.Config.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PSMS.Host.FileServerConnectionManager.Config.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8357340-7a8b-4520-beb4-3962d6a6e7e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
