6b4a4ad265fbe3ff3d5fc34eb75985c76dcddad1
MudCitizen <mudcitizen@gmail.com>
2017-11-08 08:31:56 -0500

    Scrapped unit test on Service

---
+++ PSMS.Host.FileServerConnectionManager/PSMS.Host.FileServerConnectionManager.Tests/TestService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PSMS.Host.FileServerConnectionManager;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ServiceProcess;
+
+namespace PSMS.Host.FileServerConnectionManager.Tests
+{
+    [TestClass]
+    public class TestService
+    {
+        [TestMethod]
+        public void TestCTOR()
+        {
+            ServiceBase s = new PSMS.Host.FileServerConnectionManager.Service();
+        }
+      
+    }
+}
---
+++ PSMS.Host.FileServerConnectionManager/PSMS.Host.FileServerConnectionManager.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PSMS.Host.FileServerConnectionManager.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
